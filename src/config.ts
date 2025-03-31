bb3c2495388c3b574c4e1661e53b1a2d8ebb3403
mcironlung <ramcclung@gmail.com>
2025-03-31 14:35:21 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,8 @@ export const SITE: Site = {
   title: "AstroPaper",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 10,
+  showArchives: true,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
